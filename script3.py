98292143d942ee47b65d24342ec42433dbbbd2d4
Omar Loudghiri <62630012+OmarLoud1@users.noreply.github.com>
2023-11-06 21:24:00 -0500

    Update script3.py

--- script3.py
+++ script3.py
@@ -16,7 +16,7 @@ def script3(input_file_path, output_file_path):
             querystring = {'ipAddress': line}
             headers = {
                 'Accept': 'application/json',
-                'Key': 'fc248444343aad6fb38e5b6276290e4e7b73de9740561cc88f2d55452ae8db48c15635e96935c25b'
+                'Key': ''
             }
 
             response = requests.request(method='GET', url=url, headers=headers, params=querystring)
